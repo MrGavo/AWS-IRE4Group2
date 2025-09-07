2e019bfef0ac9f32fe20cc7623a0fa6c5300a705
Wim Michels <factotum_muth@web.de>
2025-09-07 16:52:20 +0100

    Initial commit

---
+++ employee.py
@@ -0,0 +1,20 @@
+'''
+    Employee class.
+'''
+
+class Employee():
+    def __init__(self, name, an_id):
+        self.name = name
+        self.an_id = an_id
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def get_name(self):
+        return self.name
+
+    def set_id(self, new_id):
+        self.an_id = new_id
+
+    def get_id(self):
+        return self.an_id
