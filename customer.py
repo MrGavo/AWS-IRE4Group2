d70cb3db01edbdc68f836beecb9afdc7d24fa1bf
Gerardovel <gvixcot@gmail.com>
2025-09-08 12:02:30 +0100

    Gerardo commit

---
+++ customer.py
@@ -0,0 +1,16 @@
+class Customer():
+    def __init__(self,name, order):
+        self.name = name
+        self.order = order
+        self.cart = []
+
+    def add_cart(self,item):
+        self.cart.append(item)
+    
+    def show_cart(self):
+        print(self.cart)
+
+newcust = Customer("gerardo", 1)
+
+newcust.add_cart("cakes")
+newcust.show_cart()
