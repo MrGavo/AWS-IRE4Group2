616bb479cda7a6ed6dc20c5005edf5b0427071bb
Gerardovel <gvixcot@gmail.com>
2025-09-08 12:09:23 +0100

    Restored file

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import jsonio
+from Product import Cake, Drink, Book
+import customer as Customer
+
+
+def employee(name):
+    #display menu for employees
+    print(f"Welcome {name}")
+
+def customer_interface(name):
+    #display menu for cakes
+    print(f"welcome {name}")
+    cust = input("Are you interested in drinks, cakes or books? \n")
+    cust = cust.lower()
+    if cust == "drinks":
+        print("Today's drink menu is")
+    elif cust == "cakes":
+        print("Today's cakes menu is")
+        cakes = jsonio.read_cakes()
+        for k in cakes:
+            print(k, cakes[k]['description'])
+    elif cust == "books":
+        print("See what's in our book shelf")
+    else:
+        print("sorry not available today")
+
+
+
+def main():
+    while True:
+        #asks if it is the employee or the customer that will log in
+        os.system("cls")
+        person = input("Employee or Customer (E/C): ").lower()
+        #if the person is Employee, call the definition of employee
+        if person == "e":
+            os.system("cls")
+            emp = input("Please log in with your name: ")
+            employee(emp)
+            #print("employee")
+        #If the person is customer, call definition of customer
+        elif person == "c":
+            os.system("cls")
+            print("Welcome to The Coffee Stain")
+            name = input("Please enter your name: ").title()
+            customer_interface(name)
+            
+            #print ("customer")
+        else:
+            print("Invalid input, try again.")
+
+
+
+main()
