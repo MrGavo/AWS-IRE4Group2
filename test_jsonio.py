b7ecda19b03c2e72ea6cccf5d9a9cf326cd69316
GavoZen <gavin.spillane@outlook.com>
2025-09-09 15:09:17 +0100

    added discount

--- test_jsonio.py
+++ test_jsonio.py
@@ -9,6 +9,7 @@ the write lines below.
 
 
 import os
+import time
 from select import select
 from jsonio import read_books, read_cakes, read_drinks, write_books, write_cakes, write_drinks
 from Product import Cake, Drink, Book
@@ -154,21 +155,24 @@ book5 = {
 # Write sample data to JSON files
 # If you want to use this then first delete existing json files and uncomment these lines
 # If you change any of the indiviual values - eg the price of Apple Pie then write will update the json
-# write_cakes(cake1)
-# write_cakes(cake2)
-# write_cakes(cake3)
-# write_cakes(cake4)
-# write_cakes(cake5)
-# write_drinks(drink1)
-# write_drinks(drink2)
-# write_drinks(drink3)
-# write_drinks(drink4)
-# write_drinks(drink5)
-# write_books(book1)
-# write_books(book2)
-# write_books(book3)
-# write_books(book4)
-# write_books(book5)
+def reset_json():
+    write_cakes(cake1)
+    write_cakes(cake2)
+    write_cakes(cake3)
+    write_cakes(cake4)
+    write_cakes(cake5)
+    write_drinks(drink1)
+    write_drinks(drink2)
+    write_drinks(drink3)
+    write_drinks(drink4)
+    write_drinks(drink5)
+    write_books(book1)
+    write_books(book2)
+    write_books(book3)
+    write_books(book4)
+    write_books(book5)
+
+# reset_json()
 
 # Read and print data from JSON files
 # print("\nCAKES")
@@ -181,70 +185,59 @@ book5 = {
 # print(read_books())
 # print(type(read_books()))
 
-
-# Access individual Dicts
 cakes = read_cakes()
 drinks = read_drinks()
 books = read_books()
-# # print(cakes.keys())
-# for k in cakes:
-#     print(k.ljust(30), cakes[k]['description'])
-# input("What Cake would you like to buy? : ")
-order = []
-
-def menu_select(product, option):
-    if option == 1:
-        print("Adding ", list(product.keys())[0], " to your basket")
-        order.append(list(product.keys())[0])
-    elif option == 2:
-        print("Adding ", list(product.keys())[1], " to your basket")
-        order.append(list(product.keys())[1])
-    elif option == 3:
-        print("Adding ", list(product.keys())[2], " to your basket")
-        order.append(list(product.keys())[2])
-    elif option == 4:
-        print("Adding ", list(product.keys())[3], " to your basket")
-        order.append(list(product.keys())[3])
-    elif option == 5:
-        print("Adding ", list(product.keys())[4], " to your basket")
-        order.append(list(product.keys())[4])
 
 
-for i, k in enumerate(cakes, start=1):
-    print(i, k.ljust(40), cakes[k]['description'])
-selection = int(input("What Cake would you like to buy? : "))
-menu_select(cakes, selection)
+# --------------------------------------------------------------------------
+# Below is Menu System for adding products to basket
+# --------------------------------------------------------------------------
+# def menu_select(product):
+#     selection = -1
+#     product_keys = list(product.keys())
+#     num_products = len(product_keys)
 
-for i, k in enumerate(drinks, start=1):
-    print(i, k.ljust(40), drinks[k]['description'])
-selection = int(input("What Drink would you like to buy? : "))
-menu_select(drinks, selection)
+#     while selection != 0:
+#         os.system('cls' if os.name == 'nt' else 'clear')
+        
+#         # Iterating through the list of products - enumerate just puts a number beside each product
+#         for i, k in enumerate(product, start=1):
+#             print(f"{i}. {k.ljust(40)} {product[k]['description']}")
+#         print('0. To Proceed')
+        
+#         try:
+#             selection = int(input("Select item to add to your basket: "))
+
+#             #if 1 <= selection <= num_products:
+#             if selection >= 1 and selection <= num_products:
+#                 item_to_add = product_keys[selection - 1]
+#                 print(f"Adding {item_to_add} to your basket")
+#                 order.append(item_to_add) #MrGavo : This is just how i was adding to an order list - you can change
+#                 # MrGavo : Another option is to create an instance of the class
+#                 # MrGavo : You would need to pass another arg for instance type eg. Cake, Drink, Book
+#                 # item = Cake(**cakes[item_to_add])
+#             elif selection == 0:
+#                 print("Proceeding to checkout...")
+#             else:
+#                 print("Invalid selection. Please try again.")
+#         except ValueError:
+#             print("Invalid input. Please enter a number.")
+
+#         time.sleep(2)
 
 
-for i, k in enumerate(books, start=1):
-    print(i, k.ljust(40), books[k]['description'])
-selection = int(input("What Book would you like to buy? : "))
-menu_select(books, selection)
-# if selection == 1:
-#     print("Adding ", list(cakes.keys())[0], " to your basket")
-#     order.append(list(cakes.keys())[0])
-# elif selection == 2:
-#     print("Adding ", list(cakes.keys())[1], " to your basket")
-#     order.append(list(cakes.keys())[1])
-# elif selection == 3:
-#     print("Adding ", list(cakes.keys())[2], " to your basket")
-#     order.append(list(cakes.keys())[2])
-# elif selection == 4:
-#     print("Adding ", list(cakes.keys())[3], " to your basket")
-#     order.append(list(cakes.keys())[3])
-# elif selection == 5:
-#     print("Adding ", list(cakes.keys())[4], " to your basket")
-#     order.append(list(cakes.keys())[4])
+# menu_select(cakes)
+# menu_select(drinks)
+# menu_select(books)
 
+# --------------------------------------------------------------------------
+# Testing dict updates
+# --------------------------------------------------------------------------
 # print(cakes.values())
 # print(cakes['Apple Pie'])
 # print(cakes['Apple Pie']['price'])
-print (order)
+# print (order)
 
 # Read cakes.json into a dictionary of dictionaries
 # Update price of Apple Pie
@@ -253,6 +246,87 @@ print (order)
 # cakes['Apple Pie']['price'] = 33.44
 # write_cakes(cakes)
 
+
+
+
+
+# --------------------------------------------------------------------------
+# Below is Menu System for adding new Products
+# --------------------------------------------------------------------------
+# name = ""
+# price = 0
+# desc = ""
+# stock = 0
+# genre = ""
+# author = ""
+
+# def add_new_prod(item):
+#     while True:
+#         os.system('cls' if os.name == 'nt' else 'clear')
+#         print(f"Adding New {item}\n")
+#         try:
+#             name = input("Name".ljust(20) + ": ")
+#             price = float(input("Price".ljust(20) + ": "))
+#             desc = input("Description".ljust(20) + ": ")
+#             stock = int(input("Stock".ljust(20) + ": "))
+#             break
+#         except ValueError:
+#             print("Invalid input. Please enter a number.")
+#             time.sleep(1)
+#     return name.title(), price, desc.title(), stock
+
+# def add_new_cake():
+#     name, price, desc, stock = add_new_prod("Cake")
+#     new_cake = {
+#         name: {
+#             "name": name,
+#             "price": price,
+#             "description": desc,
+#             "stock": stock
+#         }
+#     }
+#     print("Cake Added")
+#     time.sleep(1)
+#     write_cakes(new_cake)
+
+
+# def add_new_drink():
+#     name, price, desc, stock = add_new_prod("Drink")
+#     new_drink = {
+#         name: {
+#             "name": name,
+#             "price": price,
+#             "description": desc,
+#             "stock": stock
+#         }
+#     }
+#     print("Drink Added")
+#     time.sleep(1)
+#     write_drinks(new_drink)
+
+
+# def add_new_book():
+#     name, price, desc, stock = add_new_prod("Book")
+#     genre = input("Genre".ljust(20) + ": ").title()
+#     author = input("Author".ljust(20) + ": ").title()
+#     new_book = {
+#         name: {
+#             "name": name,
+#             "price": price,
+#             "description": desc,
+#             "stock": stock,
+#             "genre": genre,
+#             "author": author
+#         }
+#     }
+#     print("Book Added")
+#     time.sleep(1)
+#     write_books(new_book)
+
+
+# add_new_cake()
+# add_new_drink()
+# add_new_book()
 
 
 
